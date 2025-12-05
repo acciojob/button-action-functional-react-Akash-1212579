5fb507959f4c99179d2486e1916cd3130826b77d
Akash <dalbhanjanakash1@gmail.com>
2025-12-05 20:12:50 +0530

    attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,25 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let text = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
+ const[paras,setParas] = useState([]);
+ function handlePara()
+ {  
+  setParas(prev=>[...prev,text]);
+ 
+ }
+  console.log(paras);
   return (
     <div id="main">
-      // Do not alter the main div
+      
+      <button id="click" onClick={()=>handlePara()}>Click</button>
+      
+     {
+  paras.map((ele, index) => (
+    <p id="para" key={index}>{ele}</p>
+  ))
+}
+
     </div>
   );
 }
